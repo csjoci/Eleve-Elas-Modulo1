b8ac376d65cde830d5b7365635f25ca286846aa3
Jociane Costa e Silva <jociane.silva@ambevtech.com.br>
2024-07-27 19:20:56 -0300

    adding new exercises

---
+++ Aula-24-05/Exercicio_Aula05.cs
@@ -0,0 +1,99 @@
+﻿
+int option = -1;
+Queue<string> listaPedidos = new Queue<string>();
+Stack<string> pedidosConcluidos = new Stack<string>();
+
+do
+{
+    Console.WriteLine("** Menu **");
+    Console.WriteLine("Informe uma opcão:");
+    Console.WriteLine("1 - Adicionar Pedido");
+    Console.WriteLine("2 - Preparar Pedido");
+    Console.WriteLine("3 - Visualizar pedidos pendentes");
+    Console.WriteLine("4 - Visualizar pedidos concluidos");
+    Console.WriteLine("5 - Sair");
+    option = Convert.ToInt32(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            string optionContinue = "s";
+            do
+            {
+                Console.WriteLine("Você escolheu a opcão de Adicionar Pedido.");
+                Console.WriteLine("O que gostaria de pedir?");
+                string pedido = Console.ReadLine();
+                listaPedidos.Enqueue(pedido);
+                Console.WriteLine("Gostaria de adicionar outro pedido? (s/n)");
+                optionContinue = Console.ReadLine();
+            } while (optionContinue == "s");
+            break;
+        case 2:
+            string optionContinuePrepare = "s";
+            do
+            {
+                Console.WriteLine("Você escolheu a opcão de Preparar Pedido.");
+                if (listaPedidos.Count > 0)
+                {
+                    Console.WriteLine("Os ultimos pedidos realizados foram:");
+                    foreach (var item in listaPedidos)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine("----------------");
+                    var ultimo = listaPedidos.Peek();
+                    Console.WriteLine($"deseja preparar? {ultimo} (s/n)");
+                    string order = Console.ReadLine();
+                    if (order == "s")
+                    {
+                        listaPedidos.Dequeue();
+                        pedidosConcluidos.Push(ultimo);
+                    }
+                 }
+                else
+                {
+                    Console.WriteLine("Nada há preparar! Lista de pedidos vazia!");
+                }
+                Console.WriteLine("Gostaria de preparar outro pedido? (s/n)");
+                optionContinuePrepare = Console.ReadLine();
+            } while (optionContinuePrepare == "s");
+            break;
+        case 3:
+            if (listaPedidos.Count > 0)
+            {
+                Console.WriteLine("Os ultimos pedidos realizados foram:");
+                foreach (var item in listaPedidos)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("----------------");
+            }
+            else
+            {
+                Console.WriteLine("Nada há preparar! Lista de pedidos vazia!");
+            }
+            break;
+        case 4:
+            if (pedidosConcluidos.Count > 0)
+            {
+                Console.WriteLine("Os ultimos pedidos concluidos foram:");
+                foreach (var item in pedidosConcluidos)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("----------------");
+            }
+            else
+            {
+                Console.WriteLine("Lista de concluidos vazia!");
+            }
+            break;
+
+
+        default:
+            if (option != 5)
+                Console.WriteLine("Informe uma opção valida!");
+            break;
+    }
+
+} while (option != 5);
